c372ee773e0df0c89f0a6a600501fffeac8dd552
Omar Beiruty <omar.beiruty@gmail.com>
2017-04-14 21:15:53 +0000

    Done.

--- loops.js
+++ loops.js
@@ -11,3 +11,23 @@ array.push("I am " +i+ " strange loops.");
   }
   return array;
 }
+
+function whileLoop(number){
+  while (number > 0) {
+    console.log (number);
+    number = number -1;
+  }
+  return "done"
+}
+
+function doWhileLoop(array){
+  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do { array.pop();} 
+while (array.length > 0 && maybeTrue()) 
+
+return array;
+}
